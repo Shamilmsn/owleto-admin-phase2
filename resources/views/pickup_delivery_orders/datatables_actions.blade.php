3706118d4f1912225d380169a59b5b12497390df
vedhika <vedhika143@gmail.com>
2023-05-15 22:26:26 +0400

    orders edit option removed

--- resources/views/pickup_delivery_orders/datatables_actions.blade.php
+++ resources/views/pickup_delivery_orders/datatables_actions.blade.php
@@ -1,18 +1,25 @@
 <div class='btn-group btn-group-sm'>
     @can('pickup-delivery-orders.show')
-        <a data-toggle="tooltip" data-placement="bottom" title="{{trans('lang.view_details')}}" href="{{ route('pickup-delivery-orders.show', $id) }}" class='btn btn-link'>
+        <a data-toggle="tooltip" data-placement="bottom"
+           title="{{trans('lang.view_details')}}"
+           href="{{ route('pickup-delivery-orders.show', $id) }}"
+           class='btn btn-link'>
             <i class="fa fa-eye"></i>
         </a>
     @endcan
 
-    @can('orders.edit')
-    <a data-toggle="tooltip" data-placement="bottom" title="{{trans('lang.order_edit')}}" href="{{ route('pickup-delivery-orders.edit', $id) }}" class='btn btn-link'>
-        <i class="fa fa-edit"></i>
-    </a>
-    @endcan
+{{--    @can('orders.edit')--}}
+{{--    <a data-toggle="tooltip" data-placement="bottom"
+            title="{{trans('lang.order_edit')}}"
+            href="{{ route('pickup-delivery-orders.edit', $id) }}"
+             class='btn btn-link'>--}}
+{{--        <i class="fa fa-edit"></i>--}}
+{{--    </a>--}}
+{{--    @endcan--}}
 
     @if(!$driver_id && $order_status_id != \App\Models\OrderStatus::STATUS_CANCELED)
-        <a data-toggle="tooltip" data-placement="bottom" data-id="{{ $id }}" href="#" class='btn btn-link assign-driver'>
+        <a data-toggle="tooltip" data-placement="bottom"
+           data-id="{{ $id }}" href="#" class='btn btn-link assign-driver'>
             Assign Driver
         </a>
     @endif
